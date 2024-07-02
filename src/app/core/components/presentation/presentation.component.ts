d15e7685bbfbd067c45c963e7c23b8bb669b2a15
MisaelGG11 <gg20031@ues.edu.sv>
2024-07-02 10:22:31 -0600

    feat: add presentation component ro the portfolio

---
+++ src/app/core/components/presentation/presentation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-presentation',
+  standalone: true,
+  imports: [],
+  templateUrl: './presentation.component.html',
+  styles: ``
+})
+export class PresentationComponent {
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,40 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'text': 'text 3s linear infinite', // Animación para el texto
+        'border': 'border 3s linear infinite', // Animación para el borde
+        'border-spin': 'border-spin 6s linear infinite',
+      },
+      keyframes: {
+        'border-spin': {
+          '100%': {
+            transform: 'rotate(-360deg)',
+          },
+        },
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 100%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 100%',
+            'background-position': 'right center',
+          },
+        },
+        border: {
+          '0%, 100%': {
+            'background-size': '200% 100%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 100%',
+            'background-position': 'right center',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
