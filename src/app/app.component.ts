31d59a74e80190394fe29f089af107e7658885b4
MisaelGG11 <gg20031@ues.edu.sv>
2024-07-03 13:30:20 -0600

    feat: add meta description and defered components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'my_resume';
+  private meta = inject(Meta);
+
+  constructor() {
+    this.meta.addTag({ name: 'description', content: 'Misael Gómez, +2 años de experiencia. Ingeniero de Sistemas informáticos. Desarrollador Fullstack especializado en aplicaciones web.' });
+  }
 }
