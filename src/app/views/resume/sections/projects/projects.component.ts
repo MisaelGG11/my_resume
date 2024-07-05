7e59691143c9b33b1bbc157598b64b3a4386fb90
MisaelGG11 <gg20031@ues.edu.sv>
2024-07-05 15:22:18 -0600

    feat: add link to redirect to to more projects

--- src/app/views/resume/sections/projects/projects.component.ts
+++ src/app/views/resume/sections/projects/projects.component.ts
@@ -5,7 +5,21 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './projects.component.html',
-  styles: ``
+  styles: `
+    .underline-hover::after {
+      content: '';
+      position: absolute;
+      width: 0;
+      height: 2px;
+      bottom: 0;
+      left: 0;
+      background-color: white;
+      transition: width 0.5s;
+    }
+
+    .underline-hover:hover::after {
+      width: 100%;
+    }`
 })
 export class ProjectsComponent {
 
