870c1347747f6c9503ce9312641db5ecb32af6cf
MisaelGG11 <gg20031@ues.edu.sv>
2025-06-16 15:36:32 -0600

    refactor: presentation component

--- src/app/views/resume/sections/presentation/presentation.component.ts
+++ src/app/views/resume/sections/presentation/presentation.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { HighlightPipe } from '@pipes/highlight.pipe';
 
 @Component({
   selector: 'presentation-section',
   standalone: true,
-  imports: [],
+  imports: [HighlightPipe],
   templateUrl: './presentation.component.html',
   styles: ``,
 })
@@ -11,4 +12,18 @@ export class PresentationComponent {
   showCV() {
     window.open('assets/docs/Hoja de Vida CV - Misael Gómez.pdf', '_blank');
   }
+
+  description = signal<string>(
+    `
+      👨‍💻 +2 años de experiencia. Ingeniero de Sistemas informáticos. Desarrollador Fullstack especializado
+      en aplicaciones web. 🚀 Apasionado por crear soluciones innovadoras y eficientes. 🛠️ Experto en
+      tecnologías modernas y en la implementación de buenas prácticas de desarrollo.
+    `
+  );
+
+  highlightedWords = signal<string[]>([
+    '+2 años de experiencia',
+    'Apasionado por crear soluciones innovadoras y eficientes.',
+    'tecnologías modernas'
+  ]);
 }
